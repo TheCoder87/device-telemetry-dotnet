6162afb567f54cbb2a66bebd6bf20c2181178178
mechaffin <a-machaf>
2018-02-14 14:45:31 -0800

    Adding a clarifying comment for JsoIgnore. ALso, updating traveis version

--- Services/Models/Rule.cs
+++ Services/Models/Rule.cs
@@ -9,9 +9,9 @@ namespace Microsoft.Azure.IoTSolutions.DeviceTelemetry.Services.Models
     public class Rule : IComparable<Rule>
     {
         private const string DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:sszzz";
-        [JsonIgnore]
+        [JsonIgnore] //comes from the StorageAdapter document and not the serialized rule
         public string ETag { get; set; } = string.Empty;
-        [JsonIgnore]
+        [JsonIgnore] //comes from the StorageAdapter document and not the serialized rule
         public string Id { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string DateCreated { get; set; } = DateTimeOffset.UtcNow.ToString(DATE_FORMAT);
